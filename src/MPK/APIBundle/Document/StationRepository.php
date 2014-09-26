b8752eda8198c5ef0b4b42000ff04f682540a530
Marcin <marcin.feleniak@polcode.net>
2014-09-26 11:01:34 +0200

    beautification

--- src/MPK/APIBundle/Document/StationRepository.php
+++ src/MPK/APIBundle/Document/StationRepository.php
@@ -26,44 +26,50 @@ class StationRepository extends DocumentRepository
 
     public function findStation($id)
     {
-        $date = new \DateTime();        
-        $day = $this->getDayOfWeek($date);
+        $date = new \DateTime();
 
         $station = $this->createQueryBuilder('Station')
                 ->field('id')->equals($id)
                 ->getQuery()
                 ->getSingleResult();
 
-        $result = null;
-        if ($station) {
-            $departures = [];
-            foreach ($station->getLines() as $line) {
-                foreach ($line->getDepartures() as $departure) {
-                    if ($departure->getDay() === $day && $departure->getDate()->getTimestamp() < $date->getTimestamp()) {
-                        $diff = $date->diff(new \DateTime($departure->getDate()->format('H:i')));
-                        $departures[] = [
-                            "line" => $line->getName(),
-                            "direction" => $line->getDirection(),
-                            "arrival" => $diff->i + ($diff->h * 60)
-                        ];
-                    }
-                }
-            }
-            
-            usort($departures, function($a, $b){
-                return $a["arrival"] <= $b["arrival"] ? -1 : 1;
-            });
-            
-            $result = [
-                "departures" => array_slice($departures, 0, 5),
-                "name" => $station->getName(),
-                "time" => $date,
-            ];
+        if (!$station) {
+            return null;
         }
-        return $result;
+
+        $departures = [];
+        foreach ($station->getLines() as $line) {
+            $this->filterDepartures($line, $date, $departures);
+        }
+
+        return [
+            "departures" => $departures,
+            "name" => $station->getName(),
+            "time" => $date,
+        ];
     }
 
-    
+    private function filterDepartures(Line $line, \DateTime $date, &$departures)
+    {
+        $day = $this->getDayOfWeek($date);
+
+        foreach ($line->getDepartures() as $departure) {
+            if ($departure->getDay() === $day && $departure->getDate()->getTimestamp() < $date->getTimestamp()) {
+                $diff = $date->diff(new \DateTime($departure->getDate()->format('H:i')));
+                $departures[] = [
+                    "line" => $line->getName(),
+                    "direction" => $line->getDirection(),
+                    "arrival" => $diff->i + ($diff->h * 60)
+                ];
+            }
+        }
+
+        usort($departures, function($a, $b) {
+            return $a["arrival"] <= $b["arrival"] ? -1 : 1;
+        });
+        $departures = array_slice($departures, 0, 5);
+    }
+
     private function getDayOfWeek(\DateTime $date)
     {
         switch (date('N', $date->getTimestamp())) {
