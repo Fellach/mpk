9f0e12d944ee3dfaea1b6a8a75bc59416e218b84
Marcin <marcin.feleniak@polcode.net>
2014-09-26 10:39:24 +0200

    complete api

--- src/MPK/APIBundle/Controller/StationsController.php
+++ src/MPK/APIBundle/Controller/StationsController.php
@@ -5,74 +5,35 @@ namespace MPK\APIBundle\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 
-
 class StationsController extends FOSRestController
 {
+
     /**
      * @Rest\Get("/get_stations")
      * desc.: Po lewej mamy liste mozliwych przystankow z wyszukiwarka 
      */
     public function getStationsAction()
     {
+        $stations = $this->get('doctrine_mongodb')->getRepository('MPKAPIBundle:Station')->findAllStations();
+
         return [
-            "stations" => [
-                [
-                    "id" => "111",
-                    "name" => "aaa",
-                ],
-                [
-                    "id" => "222",
-                    "name" => "bbb",
-                ],
-                [
-                    "id" => "333",
-                    "name" => "ccc",
-                ],
-            ]
+            "stations" => $stations
         ];
     }
-    
+
     /**
-     * @Rest\Get("/get_station/{id}", requirements={"id"="\d+"})
-     * @param type $id
+     * @Rest\Get("/get_station/{id}")
+     * @param string $id
      * desc.: pojawia sie nazwa wybranego przystanku, aktualna godzina oraz 5 najbliższych odjazdów (w formacie Linia | Kierunek | 5 min).
      */
     public function getStationAction($id)
     {
+        $station = $this->get('doctrine_mongodb')->getRepository('MPKAPIBundle:Station')->findStation($id);
+
         return [
-            "station" => [
-                "name" => "name" . $id,
-                "time" => new \DateTime(),
-                "departures" => [
-                    [
-                        "line" => "zxc",
-                        "direction" => "vbn",
-                        "arrival" => 5
-                    ],
-                    [
-                        "line" => "qwe",
-                        "direction" => "mbn",
-                        "arrival" => 6
-                    ],
-                    [
-                        "line" => "rty",
-                        "direction" => "xvc",
-                        "arrival" => 7
-                    ],
-                    [
-                        "line" => "yuio",
-                        "direction" => "ljk",
-                        "arrival" => 8
-                    ],
-                    [
-                        "line" => "pio",
-                        "direction" => "jhg",
-                        "arrival" => 9
-                    ],
-                    
-                ]
-            ]
+            "station" => $station
         ];
+        
     }
 
 }
