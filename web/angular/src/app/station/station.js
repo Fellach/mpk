2911bbb2caf79259a129b2c56788fde78c778487
Marcin Feleniak <marcin.feleniak@polcode.net>
2015-06-23 14:00:39 +0200

    fix interval

--- web/angular/src/app/station/station.js
+++ web/angular/src/app/station/station.js
@@ -13,22 +13,23 @@
         });
     }]);
     
-    var interval;
-    
     app.controller('StationController', function ($scope, $stateParams, $interval, ApiService) {
+        $scope.model.station = null;
         
         var init = function() {
             $scope.model = $scope.$parent.model;
             
             ApiService.getStation($stateParams.id);
             
-            $interval.cancel(interval);
-            
-            interval = $interval(function(){
+            var interval = $interval(function(){
                 if (new Date($scope.model.station.time).getMinutes() !== new Date().getMinutes()) {
                     ApiService.getStation($stateParams.id);
                 }
             }, 1000);
+            
+            $scope.$on('$destroy', function () { 
+                $interval.cancel(interval); 
+            });
         };
 
         init();
